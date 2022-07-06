5c592fe7d3144c10684c68b9e13815333d8db6b9
Connie Standish <conniestand@gmail.com>
2022-07-06 10:47:58 -0400

    pseudocode

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+//Pseudocode:
+//Objective: Create a weather app that diplays current weather data by location
+//Note: First create an OpenWeather API account and visit the API key management page
+
+//1. The weather app should call to Open Weather API's current endpoint using JavaScript Fetch API
+
+//2. To receive full credit the app must display: city/country code, Google Maps link, weather icon, description of current weather, actual temp., perceived temp., time weather info was last updated. 
+
+//3. If user enters data that does not retrieve weather data it should display 'location not found'
+
+//4. To obtain weather data from API, construct URL for request using JavaScript.
+
+//5. Add an extra parameter to ensure measurements are imperial units.
+
+//6. Use string concatenation to ensure that user entered data will be pulled. 
+
+//7. Additional steps to take when setting up icon, time updated, and Google maps link.
